c39ec439819feb8a280637e44b24f2c73e1bbfd3
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-07-07 20:26:26 +0300

    test2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import 'antd/dist/antd.min.css'
 import React from 'react'
-
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Products from './pages/products/Products';
 const App = () => {
   return (
-    <div>App</div>
+    < >
+        <Router> 
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/products" element={<Products />}/>
+        </Routes>
+        </Router>
+    </>
   )
 }
 
