062e610cd3e191ea7d39de45a9da18e41e293d7f
mert-ozturk <38337566+mert-ozturk@users.noreply.github.com>
2022-07-08 22:16:11 +0300

    test3

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Product = ({product}) => {
+  return (
+    <div>
+  {product.name}
+
+    </div>
+  )
+}
+
+export default Product
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import axios from 'axios'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import LayoutApp from './../../components/Layout.jsx';
+import {Row,Col} from 'antd'
+import Product from '../../components/Product.jsx';
 
 const Home = () => {
+
+  const [productData,setProductData] = useState([])
+
+  useEffect(()=>{
+    const getAllProducst = async () =>{
+        try{
+
+          const {data} = await axios.get('/api/products/getproducts')
+          setProductData(data);
+          console.log(data)
+
+        }catch(error){
+          console.log(error)
+        }
+    } 
+    
+    getAllProducst()
+    
+  },[])
+
   return (
-    <div>
-        Home
-    </div>
+    <LayoutApp>
+      <Row>
+        {productData.map(product =>(
+          <Col xs={24} sm={6} md={12} lg={6}>
+          <Product product={product} />
+          </Col>
+        ))}
+       
+      </Row>
+    </LayoutApp>
   )
 }
 
